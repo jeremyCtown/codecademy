8a4c0271445f6f300832fdefe8ce3784407c204a
Jeremy Crawford <jfcraw08@gmail.com>
2018-03-25 20:46:07 -0700

    taking a vacay is now complete

--- 401/intro_to_python.py
+++ 401/intro_to_python.py
@@ -1,4 +1,4 @@
-#PygLatin
+# PygLatin
 
 pyg = 'ay'
 
@@ -14,3 +14,33 @@ else:
     print 'empty'
 
 
+# Taking a vacation
+
+def hotel_cost(nights):
+  return 140 * nights
+
+def plane_ride_cost(city):
+  if city == "Charlotte":
+    return 183
+  elif city == "Tampa":
+    return 220
+  elif city == "Pittsburgh":
+    return 222
+  elif city == "Los Angeles":
+    return 475
+  else:
+    return "Nope"
+  
+def rental_car_cost(days):
+  costs = 40 * days
+  if days >= 7:
+    costs -= 50
+  elif 7> days >= 3:
+    costs -= 20
+  return costs
+
+def trip_cost(city, days, spending_money):
+	trip = rental_car_cost(days) + hotel_cost(days - 1) + plane_ride_cost(city) + spending_money
+	return trip
+
+print trip_cost("Los Angeles", 5, 600)
