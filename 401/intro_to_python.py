47793a11d289f9b01ac96b94771e549bdd474da3
Jeremy Crawford <jfcraw08@gmail.com>
2018-03-28 23:14:02 -0700

    tutorial exercises complete

--- 401/intro_to_python.py
+++ 401/intro_to_python.py
@@ -44,3 +44,134 @@ def trip_cost(city, days, spending_money):
 	return trip
 
 print trip_cost("Los Angeles", 5, 600)
+
+
+## Day at the market
+shopping_list = ["banana", "orange", "apple"]
+
+stock = {
+  "banana": 6,
+  "apple": 0,
+  "orange": 32,
+  "pear": 15
+}
+    
+prices = {
+  "banana": 4,
+  "apple": 2,
+  "orange": 1.5,
+  "pear": 3
+}
+
+def compute_bill(food):
+  total = 0
+  for item in food:
+    if stock[item] > 0:
+      stock[item] = stock[item] - 1
+      total += prices[item]
+  return total
+
+print total
+
+# Becomes the Teacher
+
+students = [lloyd, alice, tyler]
+
+lloyd = {
+  "name": "Lloyd",
+  "homework": [90.0, 97.0, 75.0, 92.0],
+  "quizzes": [88.0, 40.0, 94.0],
+  "tests": [75.0, 90.0]
+}
+alice = {
+  "name": "Alice",
+  "homework": [100.0, 92.0, 98.0, 100.0],
+  "quizzes": [82.0, 83.0, 91.0],
+  "tests": [89.0, 97.0]
+}
+tyler = {
+  "name": "Tyler",
+  "homework": [0.0, 87.0, 75.0, 22.0],
+  "quizzes": [0.0, 75.0, 78.0],
+  "tests": [100.0, 100.0]
+}
+
+def average(numbers):
+  total = sum(numbers)
+  return float(total) / len(numbers)
+
+def get_average(student):
+  homework = average(student["homework"])
+  quizzes = average(student["quizzes"])
+  tests = average(student["tests"])
+  weighted_total = homework * .1 + quizzes * .3 + tests * .6
+  return weighted_total
+
+def get_letter_grade(score):
+  if score >= 90:
+    return "A"
+  elif score >= 80:
+    return "B"
+  elif score >= 70:
+    return "C"
+  elif score >= 60:
+    return "D"
+  else:
+    return "F"
+
+def get_class_average(class_list):
+  results = []
+  for student in class_list:
+    results.append(get_average(student))
+  return average(results)
+
+print get_class_average(students)
+
+# Battleship
+
+from random import randint
+
+board = []
+
+for x in range(5):
+  board.append(["O"] * 5)
+
+def print_board(board):
+  for row in board:
+    print " ".join(row)
+
+print_board(board)
+
+def random_row(board):
+  return randint(0, len(board) - 1)
+
+def random_col(board):
+  return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+print ship_row
+print ship_col
+
+for turn in range(4):
+  print "Turn", turn + 1
+
+  guess_row = int(raw_input("Guess Row: "))
+  guess_col = int(raw_input("Guess Col: "))
+
+  if guess_row == ship_row and guess_col == ship_col:
+    print "Congratulations! You sunk my battleship!"
+    break
+  else:
+    if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+      print "Oops, that's not even in the ocean."
+    elif(board[guess_row][guess_col] == "X"):
+      print "You guessed that one already."
+    else:
+      print "You missed my battleship!"
+      board[guess_row][guess_col] = "X"
+
+    if turn == 3:
+      print "Game Over"
+    else:
+      print_board(board)
